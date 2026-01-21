e6375559679dabdf1344948fb96a47c04d7fb942
Denvey <denvey050679@gmail.com>
2026-01-21 12:23:51 +0500

    * Добавление в .env для s3 доменов и региона

--- infra/s3/minio/create_bucket.sh
+++ infra/s3/minio/create_bucket.sh
@@ -12,17 +12,17 @@ echo "MinIO server is up. Alias 'myminio' configured."
 if mc ls myminio/${S3_PUBLIC_BUCKET} > /dev/null 2>&1; then
     echo "Public bucket '${S3_PUBLIC_BUCKET}' already exists."
 else
-    mc mb myminio/${S3_PUBLIC_BUCKET}
+    mc mb --region ${S3_REGION} myminio/${S3_PUBLIC_BUCKET}
     mc anonymous set download myminio/${S3_PUBLIC_BUCKET}
-    echo "Public bucket '${S3_PUBLIC_BUCKET}' created and set to public."
+    echo "Public bucket '${S3_PUBLIC_BUCKET}' created in region '${S3_REGION}' and set to public."
 fi
 
 #Приватный
 if mc ls myminio/${S3_PRIVATE_BUCKET} > /dev/null 2>&1; then
     echo "Private bucket '${S3_PRIVATE_BUCKET}' already exists."
 else
-    mc mb myminio/${S3_PRIVATE_BUCKET}
-    echo "Private bucket '${S3_PRIVATE_BUCKET}' created (access restricted)."
+    mc mb --region ${S3_REGION} myminio/${S3_PRIVATE_BUCKET}
+    echo "Private bucket '${S3_PRIVATE_BUCKET}' created in region '${S3_REGION}' (access restricted)."
 fi
 
 echo "MinIO setup completed."
